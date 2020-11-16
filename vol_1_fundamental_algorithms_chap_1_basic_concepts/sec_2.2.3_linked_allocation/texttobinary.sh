51a7997f7b140b8df8655329967d02d19ff0143d
zmajeed <zmajeed@users.noreply.github.com>
2020-11-16 07:10:40 -0500

    handle 64-bit two's complement negative numbers, add debug option, allow whitespace or comma delimiters

--- vol_1_fundamental_algorithms_chap_1_basic_concepts/sec_2.2.3_linked_allocation/texttobinary.sh
+++ vol_1_fundamental_algorithms_chap_1_basic_concepts/sec_2.2.3_linked_allocation/texttobinary.sh
@@ -2,20 +2,24 @@
 
 # texttobinary.sh
 
-# echo "0,257,9,2,3,7,7,5,5,8,8,6,4,6,1,3,7,4,9,5,2,8,0,0"|
-# texttobinary.sh |
-# od -An -td4 -w4 -v
-
 function usage {
-  echo "usage: texttobinary.sh [-w width] [-e endian] < in.txt > out.dat"
+  echo "usage: texttobinary.sh [-d] [-w width] [-e endian] < in.txt > out.dat"
   echo "converts comma-separated numbers on stdin to binary data on stdout"
+  echo "-d: debug print string of hex escapes"
   echo "-w: width in bytes of binary word, default is 4"
   echo "-e: endian format big|little, default is little"
-  echo "in.txt: numbers separated by commas without whitespace"
+  echo "in.txt: numbers separated by commas or whitespace"
+  echo "examples:"
+  echo
+  echo "texttobinary.sh -d < <(echo -e \"9, 2, 3, -7\n-5 100 1024\")"
+  echo "\x09\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00\xf9\xff\xff\xff\xfb\xff\xff\xff\x64\x00\x00\x00\x00\x04\x00\x00"
+  echo
+  echo "echo \"9, 2, 3, -7\n-5 100 1024\" | texttobinary.sh | od -An -td4 -w4 -v"
 }
 
-while getopts "e:hw:" opt; do
+while getopts "de:hw:" opt; do
   case $opt in
+    d) debug=true;;
     e) endian=$OPTARG;;
     h) usage; exit 0;;
     w) width=$OPTARG;;
@@ -24,15 +28,26 @@ while getopts "e:hw:" opt; do
 done
 shift $((OPTIND-1))
 
-: ${width:=4} ${endian:=little}
+: ${width:=4} ${endian:=little} ${debug:=false}
 
 [[ $endian != big && $endian != little ]] && usage && exit 1
 
 let len=2*width
 
-while IFS=, read -a vals; do
+while IFS+=, read -a vals; do
   for((i=0; i < ${#vals[*]}; ++i)); do
-    printf -vhex "%.${len}x" ${vals[i]}
+    val=${vals[i]}
+
+# printf output is 64-bit bigendian
+#printf "0x%.2x" -7 prints 0xfffffffffffffff9
+    printf -vhex "%.${len}x" $val
+
+# truncate sign extension from right for 64-bit negative numbers past maximum length eg -7 should be 0xfffffff9 for 4 bytes
+    if ((val < 0)); then
+      hex=${hex: -len}
+    fi
+
+# reverse bytes for littleendian
     if [[ $endian == little ]]; then
       for((j=0; j < ${#hex}; j+=2)); do
         str+="\\x${hex: -j-2:2}"
@@ -42,8 +57,16 @@ while IFS=, read -a vals; do
         str+="\\x${hex:j:2}"
       done
     fi
+
   done
 done
 
+# prints hex escaped string for debugging
+if $debug; then
+  echo "$str"
+  exit 0
+fi
+
+# print binary data
 echo -en "$str"
 
